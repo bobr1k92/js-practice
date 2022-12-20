4c1f2ffb9a18edf650be71f07937363d2712f939
Denis Mazurov <19bobrik92@gmail.com>
2022-12-20 23:54:19 +0200

    search-news

---
+++ src/js/search-news.js
@@ -0,0 +1,57 @@
+
+/**
+ * Одна стаття має
+ * 1. Заголовок
+ * 2. Опис
+ * 3. Картинку
+ * 4. Посилання на статтю
+ */
+
+/**
+ * 1. Додати сlick listener до кнопки пошуку
+ * 2. Написати функцію відправки запиту і обробки відповіді 
+ * 3. Написати функію рендеру
+ */
+
+const API_KEY = 'b5f9a10038a94cda93723e0a1eeb5c6e';
+
+const searchBtnRef = document.getElementById('searchCtr');
+const searchField = document.getElementById('searchNewsField');
+const articlesContainer = document.getElementById('articles');
+
+searchBtnRef.addEventListener('click', e => {
+    getNews ({
+        query: searchField.value
+    })
+    console.log(query);
+});
+
+function getNews({query}) {
+    const urlAPI = `https://newsapi.org/v2/everything?q=${query}&from=2022-12-12&apiKey=${API_KEY}`
+
+    fetch(urlAPI).then(res => {
+    if (!res.ok) {
+        throw new Error(res.message)
+    }
+    return res.json()
+  })
+  .then(({articles}) => {
+    render(articles);  
+  })
+}
+
+function render(articles) {
+    articlesContainer.innerHTML = '';
+
+    const articlesElement = articles.map(({title, description, url, urlToImage}) => {
+        return `<article class="news">
+        <h2>${title}</h2>
+        <div>${description}</div>
+        <img src="${urlToImage}" alt="${title}" />
+        <a href="${url}">more</a>
+      </article>`;    
+    });
+
+    articlesContainer.insertAdjacentHTML('beforeend', articlesElement.join(''));
+}
+ 
